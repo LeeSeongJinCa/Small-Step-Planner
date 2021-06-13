24cdfbf00acb8cea8eab5a98b63b594fe110b33d
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-06-13 23:46:11 +0900

    update : add functions
    getLastDate, getLocalDateKey, getLocalDateUntilMonth

--- src/utils/functions/getLocalDate.ts
+++ src/utils/functions/getLocalDate.ts
@@ -1,5 +1,30 @@
 import padNum from "./padNum";
 
+const date = new Date();
+const y = date.getFullYear();
+const m = date.getMonth() + 1;
+
+export const getLastDate = () => {
+  const lastDate = new Date(y, m, 0).getDate();
+
+  return lastDate;
+};
+
+export const getLocalDateKey = (i: number) => {
+  const date = new Date();
+  const y = date.getFullYear();
+  const m = date.getMonth() + 1;
+
+  return getLocalDate(new Date(y, m - 1, i + 1));
+};
+
+export const getLocalDateUntilMonth = (date: Date) => {
+  const y = date.getFullYear();
+  const m = date.getMonth() + 1;
+
+  return `${y}-${padNum(m)}`;
+};
+
 const getLocalDate = (date: Date) => {
   const y = date.getFullYear();
   const m = date.getMonth() + 1;
