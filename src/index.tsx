015ec00685ba80320c65a824bcdaf38cc8e469ef
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-06-13 23:49:00 +0900

    update : add Router and RecoilRoot

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 
 import "./reset.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <Router>
+        <App />
+      </Router>
+    </RecoilRoot>
   </React.StrictMode>,
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
