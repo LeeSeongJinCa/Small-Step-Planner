b22b0759786ffc9aeb4e331f44c3d438a7f3e14b
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-06-14 23:43:46 +0900

    add(assets) : barrel file for assets

---
+++ src/components/assets/index.ts
@@ -0,0 +1,9 @@
+export {
+  AiFillCheckSquare,
+  AiFillDelete,
+  AiOutlineEnter,
+} from "react-icons/ai";
+export { BiAddToQueue } from "react-icons/bi";
+export { CgCalendarDates, CgClose } from "react-icons/cg";
+export { FaPlus } from "react-icons/fa";
+export { GrSteps } from "react-icons/gr";
