0a6ef7f89fe699a7dd3059e97758d50267714504
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-06-14 23:47:41 +0900

    update(App) : add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { Route, Switch } from "react-router";
-import { Link } from "react-router-dom";
 
-import { Planner, Manage } from "./pages";
+import { Planner, Manage, Header } from "./pages";
 
 const App = () => {
   return (
     <>
-      <Link to="/">/home</Link>
+      <Header />
       <Switch>
         <Route exact path="/:date" component={Planner} />
         <Route exact path="/" component={Manage} />
