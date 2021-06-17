7b62574f5957e98d0a12e459625fa273ee87fa3f
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-06-17 09:53:42 +0900

    fix : publicPath

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,6 @@
 module.exports = {
-  devServer: function (configFunction) {
-    return function (proxy, allowedHost) {
-      const config = configFunction(proxy, allowedHost);
-      config.publicPath = "/Small-Step-Planer/";
-      return config;
-    };
+  webpack: function (config, env) {
+    config.publicPath = "/Small-Step-Planner";
+    return config;
   },
 };
